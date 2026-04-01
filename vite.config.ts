e3c59d1fa4760b86d1faac746f375d1384ccc25d
dev-lan <devinscottlange@gmail.com>
2026-04-01 13:07:59 -0600

    update deploy to subdomain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
-  base: '/SpoilerFreeBaseballStatus/',
+  base: '/',
   plugins: [
     vue({ template: { transformAssetUrls } }),
     quasar({
